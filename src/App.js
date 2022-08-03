4a42ab07ab668a6209b127ac32531eb1e7d2e6ba
cdfoye <cdfoye@gmail.com>
2022-08-03 15:58:35 -0600

    set upp App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
-import BucketList from './components/BucketList';
+import { useContext } from "react";
+import { darkMode } from "./contexts/theme";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Portfolio from "./components/Projects/Projects";
+import Resume from "./components/Skills/Skills";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [{ darkMode }] = useContext(darkMode);
+
   return (
-    <div className="bucket-app">
-      <BucketList />
+    <div id="top" className={`${darkMode} app`}>
+      <Header />
+
+      <main>
+        <About />
+        <Portfolio />
+        <Contact />
+        <Resume />
+      </main>
+
+      <ScrollToTop />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
