2e149849ded84b4580e9b2c535d60305c782c23f
cdfoye <cdfoye@gmail.com>
2022-08-05 01:35:36 -0600

    fixed navbar styling and added responsive styling to contact page

--- src/components/Contact/Icons.js
+++ src/components/Contact/Icons.js
@@ -5,21 +5,21 @@ const Icons = () => {
   return (
     <div className="icon-bar">
       <a href="mailto:cdfoye@gmail.com">
-        <i class="fa fa-envelope"></i>
+        <i className="fa fa-envelope"></i>
       </a>
       <a
         href="https://github.com/cdfoye"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i class="fa fa-github"></i>
+        <i className="fa fa-github"></i>
       </a>
       <a
         href="https://www.linkedin.com/in/catherine-foye"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i class="fa fa-linkedin"></i>
+        <i className="fa fa-linkedin"></i>
       </a>
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,9 +6,6 @@ const Navbar = () => {
   return (
     <nav className="center nav topnav">
       <ul className="nav__list">
-        <Link to="/">
-          <h1>CF</h1>
-        </Link>
         <li className="nav__list-item">
           <Link to="/download" className="link link--nav">
             resume
@@ -30,6 +27,11 @@ const Navbar = () => {
         <li className="nav__list-item">
           <Link to="/about" className="link link--nav">
             about
+          </Link>
+        </li>
+        <li className="nav__list-home">
+          <Link to="/" className="link link--nav">
+            CF
           </Link>
         </li>
       </ul>
